2c16a4ba134e27a5c19ada28b45fa0a02cecc65a
Shlok Motwani <shlok.codes@gmail.com>
2025-11-03 22:05:46 +0530

    Synchronisation using explicit locks

--- MultiThreading/src/Main.java
+++ MultiThreading/src/Main.java
@@ -1,11 +1,18 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Running thread: " + Thread.currentThread().getName());
-        for(int i=1; i<=100; i++){
-            Thread t = new Thread(new PrintNumber(i));
-            t.start();
+        ExecutorService es = Executors.newCachedThreadPool();
+        for(int i=1; i<=10000; i++){
+            PrintNumber pn = new PrintNumber(i);
+            if(i == 8000){
+                System.out.println("Reached iteration #80");
+            }
+            es.execute(pn);
         }
     }
 }
