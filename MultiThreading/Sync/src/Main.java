a34b8e491e2641eb032149727d4647a992c7158c
Shlok Motwani <shlok.codes@gmail.com>
2025-11-06 07:27:36 +0530

    multithreading

--- MultiThreading/Sync/src/Main.java
+++ MultiThreading/Sync/src/Main.java
@@ -1,17 +1,19 @@
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newCachedThreadPool();
-        for(int i=0; i<100000; i++) {
+        for(int i=0; i<10000; i++) {
             Task task = new Task(i);
-            if(i == 90000){
+            if(i == 5000){
                 System.out.println();
             }
-            executor.execute(task);
+            executor.submit(task);
         }
     }
 }
--- MultiThreading/Sync/src/Task.java
+++ MultiThreading/Sync/src/Task.java
@@ -1,12 +1,16 @@
-public class Task implements Runnable {
+import java.util.concurrent.Callable;
+
+public class Task implements Callable<Void> {
     private int x;
 
     public Task(int x){
         this.x = x;
     }
 
-    public void run(){
+    public Void call() throws Exception{
         System.out.println(String.format("Task x is running on %s", Thread.currentThread().getName()));
         System.out.println(String.format("The value of x is: %s", x));
+
+        return null;
     }
 }
