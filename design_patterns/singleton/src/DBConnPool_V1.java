2a4090ee168ff4a8c9dcb0391721762b785ef4b0
Shlok Motwani <shlok.codes@gmail.com>
2025-11-12 11:11:26 +0530

    added implementation details in the comments

--- design_patterns/singleton/src/DBConnPool_V1.java
+++ design_patterns/singleton/src/DBConnPool_V1.java
@@ -1,4 +1,8 @@
-// implementation of a class having a static object
+ /* implementation of a class having a static object (eager implementation)
+ * this implementation is thread-safe, but the instance of 'DBConnPool_V1' gets created eagerly, irrespective of
+ * whether it is needed or not.
+ * Also, there cannot be any configurations implemented with the instantiation of the 'DBConnPool_V1' object.
+ */
 
 public class DBConnPool_V1 {
     private static final DBConnPool_V1 instance = new DBConnPool_V1();
