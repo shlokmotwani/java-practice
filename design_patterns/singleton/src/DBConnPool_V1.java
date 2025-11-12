5233789595291f9ce694e20cf4ca7da8e54b768e
Shlok Motwani <shlok.codes@gmail.com>
2025-11-12 11:39:58 +0530

    fix: added constructor in singleton-v1

--- design_patterns/singleton/src/DBConnPool_V1.java
+++ design_patterns/singleton/src/DBConnPool_V1.java
@@ -7,6 +7,10 @@
 public class DBConnPool_V1 {
     private static final DBConnPool_V1 instance = new DBConnPool_V1();
 
+    private DBConnPool_V1(){
+        // implementation logic
+    }
+
     public static DBConnPool_V1 getInstance(){
         return instance;
     }
