79c12c5ec69ec516a8875ea3c8dc286d5ce60be5
Shlok Motwani <shlok.codes@gmail.com>
2025-11-12 11:37:28 +0530

    fix: static instance in singleton-v3

--- design_patterns/singleton/src/DBConnPool_V3.java
+++ design_patterns/singleton/src/DBConnPool_V3.java
@@ -3,18 +3,18 @@
 // This implementation is thread-safe, but has a poor implementation of thread-safety due to the synchronized method.
 
 public class DBConnPool_V3 {
-    private DBConnPool_V3 instance;
+    private static DBConnPool_V3 instance;
 
     private DBConnPool_V3(){
-        this.instance = new DBConnPool_V3();
+        // implementation logic goes here
     }
 
     // Multiple threads cannot enter this critical section due to the 'synchronized' method.
     // This method keeps the threads waiting, defeats the purpose of multithreading and is slow!
-    public synchronized DBConnPool_V3 getInstance(){
-        if(this.instance == null){
-            this.instance = new DBConnPool_V3();
+    public static synchronized DBConnPool_V3 getInstance(){
+        if(instance == null){
+            instance = new DBConnPool_V3();
         }
-        return this.instance;
+        return instance;
     }
 }
