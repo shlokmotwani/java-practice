bc2c4f38ffa8df85a7f1f756cc660b83b085c82e
Shlok Motwani <shlok.codes@gmail.com>
2025-11-12 11:34:27 +0530

    fix: static instance in singleton v4

--- design_patterns/singleton/src/DBConnPool_V4.java
+++ design_patterns/singleton/src/DBConnPool_V4.java
@@ -6,27 +6,27 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class DBConnPool_V4 {
-    private DBConnPool_V4 instance;
+    private static DBConnPool_V4 instance;
+    private static final Lock lock = new ReentrantLock();
 
     private DBConnPool_V4(){
-        this.instance = new DBConnPool_V4();
+        // initialization logic
     }
 
-    public DBConnPool_V4 getInstance(){
+    public static DBConnPool_V4 getInstance(){
         // Multiple threads can enter this critical section.
         // This is to ensure that the application does not become synchronized (and slow!).
 
-        if(this.instance == null){ // Only the initial few threads will be able to enter this block, and the rest will divert from here.
+        if(instance == null){ // Only the initial few threads will be able to enter this block, and the rest will divert from here.
             // apply lock
             // this ensures that we allow only one of the threads to enter the block to create an instance of 'DBConnPool_V4'
-            Lock lock = new ReentrantLock();
             lock.lock();
-            if(this.instance == null) {
-                this.instance = new DBConnPool_V4();
+            if(instance == null) {
+                instance = new DBConnPool_V4();
             }
             // release lock
             lock.unlock();
         }
-        return this.instance;
+        return instance;
     }
 }
