245782517f0a070aac1ac81f76c96bb9b2c8fad8
Shlok Motwani <shlok.codes@gmail.com>
2026-01-14 13:50:19 +0530

    test level-order traversal with a input

--- DSA/trees/src/LevelOrder.java
+++ DSA/trees/src/LevelOrder.java
@@ -1,14 +1,11 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
+import java.util.*;
 
 public class LevelOrder {
-    public int[][] solve(TreeNode A) {
+    public static int[][] levelOrderTraversal(TreeNode A) {
         List<int[]> result = new ArrayList<>();
         Deque<TreeNode> q = new ArrayDeque<>();
         q.offerLast(A);
-        while(q.size() != 0){
+        while(!q.isEmpty()){
             int size = q.size();
             int[] temp = new int[size];
             for(int i=0; i<size; i++){
@@ -25,5 +22,25 @@ public class LevelOrder {
         }
 
         return result.toArray(new int[result.size()][]);
+    }
+
+    public static void main(String[] args) {
+        TreeNode ten = new TreeNode(10);
+        TreeNode five = new TreeNode(5);
+        TreeNode fifteen = new TreeNode(15);
+        TreeNode three = new TreeNode(3);
+        TreeNode eight = new TreeNode(8);
+        TreeNode twelve = new TreeNode(12);
+
+        ten.left = five;
+        ten.right = fifteen;
+        five.left = three;
+        five.right = eight;
+        fifteen.right = twelve;
+
+        int[][] postOrderResult = levelOrderTraversal(ten);
+        for(int[] temp: postOrderResult){
+            System.out.println(Arrays.toString(temp));
+        }
     }
 }
