2110aa03f204032fa3ecef514b9b5df01651d37a
Shlok Motwani <shlok.codes@gmail.com>
2026-05-21 18:35:55 +0530

    Commutable islands

---
+++ DSA/graph/src/CommutableIslands.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class CommutableIslands {
+    public int solve(int A, int[][] B) {
+        int M = B.length;
+        ArrayList<Pair>[] graph = new ArrayList[A+1];
+        for(int i=0; i<=A; i++){
+            graph[i] = new ArrayList<>();
+        }
+        for(int i=0; i<M; i++){
+            int curr = B[i][0];
+            int nbr = B[i][1];
+            int weight = B[i][2];
+
+            graph[curr].add(new Pair(nbr, weight));
+            graph[nbr].add(new Pair(curr, weight));
+        }
+
+        int ans = 0;
+        boolean[] visited = new boolean[A+1];
+        visited[1] = true;
+
+        PriorityQueue<Pair> heap = new PriorityQueue<>((a, b) -> a.weight - b.weight);
+        for(Pair pair: graph[1]){
+            heap.add(pair);
+        }
+
+        while(!heap.isEmpty()){
+            Pair curr = heap.remove();
+            if(visited[curr.vertex]){
+                continue;
+            }
+            visited[curr.vertex] = true;
+            ans += curr.weight;
+            for(Pair pair: graph[curr.vertex]){
+                if(!visited[pair.vertex]){
+                    heap.add(pair);
+                }
+            }
+        }
+        for(int i=1; i<=A; i++){
+            if(!visited[i]){
+                return -1;
+            }
+        }
+        return ans;
+    }
+
+    class Pair{
+        int vertex;
+        int weight;
+
+        Pair(int vertex, int weight){
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+    }
+}
