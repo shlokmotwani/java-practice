aca0c3e0c96d77fea223523b3db90a0a1e093142
Shlok Motwani <shlok.codes@gmail.com>
2026-06-05 02:09:00 +0530

    valid palindrome - optimised

--- DSA/neetcode/src/ValidPalindrome.java
+++ DSA/neetcode/src/ValidPalindrome.java
@@ -1,21 +1,20 @@
 public class ValidPalindrome {
     public boolean isPalindrome(String s) {
+        int N = s.length();
         s = s.toLowerCase();
         String bad = "!@$#%^&*()_ +=-{}[];:',<.>/|\\\"?";
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<s.length(); i++){
-            char curr = s.charAt(i);
-            if(!bad.contains(String.valueOf(curr))){
-                sb.append(String.valueOf(curr));
-            }
-        }
-        String str = sb.toString();
-        int N = str.length();
+
         int i=0;
         int j = N-1;
 
         while(i < j){
-            if(str.charAt(i) == str.charAt(j)){
+            while(i<j && bad.contains(String.valueOf(s.charAt(i)))){
+                i++;
+            }
+            while(i<j && bad.contains(String.valueOf(s.charAt(j)))){
+                j--;
+            }
+            if(s.charAt(i) == s.charAt(j)){
                 i++;
                 j--;
             }
