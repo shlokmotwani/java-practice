2b6317391aff4cac43966f1b75ce25c877bc389d
Shlok Motwani <shlok.codes@gmail.com>
2026-06-05 02:10:50 +0530

    contains duplicate

---
+++ DSA/neetcode/src/ContainsDuplicate.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num: nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
