57b960032a89a7bd4b16ea64e6812d245a4f08cd
Shlok Motwani <shlok.codes@gmail.com>
2026-06-05 03:44:04 +0530

    Top K Frequent Elements - optimised

--- DSA/neetcode/src/TopKFrequentElements.java
+++ DSA/neetcode/src/TopKFrequentElements.java
@@ -4,14 +4,16 @@ import java.util.PriorityQueue;
 public class TopKFrequentElements {
     public int[] topKFrequent(int[] nums, int k) {
         int N = nums.length;
-        PriorityQueue<Pair> q = new PriorityQueue<>((a, b) -> b.freq - a.freq);
+        PriorityQueue<Pair> q = new PriorityQueue<>((a, b) -> a.freq - b.freq);
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<N; i++){
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-        for(int i=0; i<N; i++){
-            q.add(new Pair(nums[i], map.getOrDefault(nums[i], 0)));
-            map.remove(nums[i]);
+        for(int num: map.keySet()){
+            q.add(new Pair(num, map.get(num)));
+            if(q.size() > k){
+                q.remove();
+            }
         }
         int[] result = new int[k];
         for(int i=0; i<k; i++){
