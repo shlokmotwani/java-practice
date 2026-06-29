cc7aaf3ab7489e443c42f4f96f2b77628c03eaf1
Shlok Motwani <shlok.codes@gmail.com>
2026-06-29 20:42:30 +0530

    ExecutorService execution

--- MultiThreading/src/Main.java
+++ MultiThreading/src/Main.java
@@ -4,11 +4,15 @@ import java.util.concurrent.Executors;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Running thread: " + Thread.currentThread().getName());
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
 
         for(int i=1; i<=100; i++){
+            if(i == 80){
+                System.out.println();
+            }
             PrintNumber pn = new PrintNumber(i);
-            Thread t = new Thread(pn);
-            t.start();
+            System.out.println("i = " + i + " ");
+            executorService.execute(pn);
         }
     }
 }
