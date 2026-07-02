82761ae73ff6ed70e27444f793d181fc30aa9809
Shlok Motwani <shlok.codes@gmail.com>
2026-07-02 13:14:42 +0530

    make SoldItemBuilder an inner class

--- design_patterns/Builder_V1/src/main/java/org/example/SoldItem.java
+++ design_patterns/Builder_V1/src/main/java/org/example/SoldItem.java
@@ -8,7 +8,7 @@ public class SoldItem {
     private int deliveryCharge;
     private String notes;
 
-    public SoldItem(SoldItemBuilder builder){
+    private SoldItem(SoldItemBuilder builder){
         this.name = builder.getName();
         this.price = builder.getPrice();
         this.quantity = builder.getQuantity();
@@ -25,5 +25,72 @@ public class SoldItem {
     public String toString() {
         return String.format("%d units of %s bought at $%d per unit, discount applied: %d, delivery charge: $%d, NOTES: %s",
                 quantity, name, price, discount, deliveryCharge, notes);
+    }
+
+    public static class SoldItemBuilder {
+        private String name;
+        private int price;
+        private int quantity;
+        private int discount;
+        private int deliveryCharge;
+        private String notes;
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public int getDiscount() {
+            return discount;
+        }
+
+        public int getDeliveryCharge() {
+            return deliveryCharge;
+        }
+
+        public String getNotes() {
+            return notes;
+        }
+
+        public SoldItemBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public SoldItemBuilder setPrice(int price) {
+            this.price = price;
+            return this;
+        }
+
+        public SoldItemBuilder setQuantity(int quantity) {
+            this.quantity = quantity;
+            return this;
+        }
+
+        public SoldItemBuilder setDiscount(int discount) {
+            this.discount = discount;
+            return this;
+        }
+
+        public SoldItemBuilder setDeliveryCharge(int deliveryCharge) {
+            this.deliveryCharge = deliveryCharge;
+            return this;
+        }
+
+        public SoldItemBuilder setNotes(String notes) {
+            this.notes = notes;
+            return this;
+        }
+
+        public SoldItem build(){
+            return new SoldItem(this);
+        }
     }
 }
